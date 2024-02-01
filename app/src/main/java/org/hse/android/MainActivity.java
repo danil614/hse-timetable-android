973ee9d9b06088e9e19721576fc4fc5875543fc5
Danil Fedoseev <fs159@mail.ru>
2024-02-01 12:14:00 +0500

    Начал делать base project

--- app/src/main/java/org/hse/android/MainActivity.java
+++ app/src/main/java/org/hse/android/MainActivity.java
@@ -4,14 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "HSE_INFORMATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("HSE", "On Create");
+        View buttonStudent = findViewById(R.id.buttonStudent);
+        View buttonTeacher = findViewById(R.id.buttonTeacher);
+
+        buttonStudent.setOnClickListener(v -> showStudent());
+        buttonTeacher.setOnClickListener(v -> showTeacher());
+
+        Log.i(LOG_TAG, "On Create");
+    }
+
+    private void showStudent() {
+        showToast("Страница для студентов");
+    }
+
+    private void showTeacher() {
+        showToast("Страница для преподавателей");
+    }
+
+    private void showToast(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
